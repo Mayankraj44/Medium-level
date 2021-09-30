88cae2bf3a9d6b63e66e64749ce4b5ebbbf16a1f
Mayankraj44 <lui.chang420@gmail.com>
2021-09-30 18:45:18 +0530

    Optimisation in Account.js

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -2,71 +2,70 @@ import React from 'react'
 import { useEffect , useState} from 'react/cjs/react.development'
 
 export const Account = () => {
-    // const [amount, setAmount] = useState('')
-    let amount;
+    const [amount, setAmount] = useState('')
+    const [add,setAdd]=useState('');
+    const [buy,setBuy]=useState('');
     useEffect(()=>{
-        amount=document.getElementById("amount");
+        
         let saving=localStorage.getItem("saving")
         if(!saving)
         {
             localStorage.setItem("saving",0)
             
-            amount.innerText="0"
+            setAmount(0);
         }
         else
         {
-            amount.innerText=saving
+            setAmount(saving);
         }
        
         // setAmount(0)
     },[])
-    const add=(e)=>{
-       let  value=Number(e.target.parentNode.children[0].value);
-       console.log(value)
-       if(value>0)
+    const Add=()=>{
+       if(add>0)
        {
         let saving=Number(localStorage.getItem("saving"))
-        console.log(typeof(saving))
-        value=value+saving
+        // console.log(typeof(saving))
+        let value=Number(add)+saving
         localStorage.setItem("saving",value)
-        amount.innerText=value
-        e.target.parentNode.children[0].value=""
+        setAmount(value);
+        
        }
        else
        {
            alert("cant add neagtive amount")
        }
+       setAdd('')
     }
-    const buy=(e)=>{
-        let  value=Number(e.target.parentNode.children[0].value);
-        console.log(value)
-        if(value>=0){
+    const Buy=()=>{
+        
+        if(buy>=0){
             let saving=Number(localStorage.getItem("saving"))
-        console.log(typeof(saving))
-        value=saving-value
+        // console.log(typeof(saving))
+        let value=saving-Number(buy)
         if(value<0)
         alert("Dont have enough money to buy this")
         else{
             localStorage.setItem("saving",value)
-            amount.innerText=value
+            setAmount(value)
         }
-        e.target.parentNode.children[0].value=""
+        
         }
         else
         alert("cant buy in negative amount")
-
+        setBuy('')
      }
     return (
         <div>
             
            <span>Amount</span> <h1 id="amount">{amount}</h1>
             <div>
-            <input type="number" />
-            <button onClick={(e)=>add(e)}>ADD</button>
+            <input type="number" onChange={(e) =>setAdd(e.target.value)} value={add}/>
+            <button onClick={Add}>ADD</button>
             </div>
             <div>
-            <input type="number" />
-            <button onClick={e=>buy(e)}> Buy</button>
+            <input type="number" onChange={(e) =>setBuy(e.target.value)} value={buy}/>
+            <button onClick={Buy}> Buy</button>
             </div>
             
         </div>
